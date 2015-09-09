49cbe04bad816beb9ed55cf42d36ee30a5c34049
duy <duy@strayharbor.com>
2015-09-08 17:46:39 -0700

    Adding second hook to set URL scheme
    The second hook sets the wsgi.url_scheme environment variable to HTTPS,
    which enables redirects to go to HTTPS URLs instead of HTTP URLs.

--- bottle_sslify.py
+++ bottle_sslify.py
@@ -14,16 +14,20 @@ class SSLify(object):
 
     before_request_decorator = self.app.hook('before_request')
     before_request_decorator(self.https_redirect)
+    before_request_decorator(self.url_scheme) # Must come after https_redirect hook
 
 
   def https_redirect(self):
     '''Redirect incoming HTTPS requests to HTTPS'''
 
     forwarded_protocol = request.get_header('X-Forwarded-Proto', '').lower()
-    request_url = request.url.lower()
 
-    if forwarded_protocol != 'https' and request_url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301 if self.permanent else 302
+    if forwarded_protocol != 'https' and request.url.startswith('http://'):
+      url = request.url.replace('http://', 'https://', 1)
+      code = 301 if self.permanent else 302
 
-        redirect(url, code=code)
+      redirect(url, code=code)
+
+  def url_scheme(self):
+    '''Set the URL scheme to https'''
+    request.environ['wsgi.url_scheme'] = 'https'
